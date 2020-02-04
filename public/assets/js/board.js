6b1d8241ce2a321c4dc286ac15f87aeb8c3ad5f9
Clark-Griswold <dustindavis1@gmail.com>
2020-02-04 19:36:55 +0000

    removing users from boards

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -34,7 +34,9 @@ function getBoard(id) {
     renderBoard();
   })
   .catch(function(err) {
-    location.replace('/boards');
+    if (err.statusText === 'Unauthorized') {
+      location.replace('/boards');
+    }
   });
 }
 
@@ -113,7 +115,14 @@ function renderBoard() {
 
 function renderContributors() {
   let $contributorListItems = board.users.map(function(user) {
-    let $contributorListItem = $('<li>').text(user.email);
+    let $contributorListItem = $('<li>');
+    let $contributorSpan = $('<span>').text(user.email);
+    let $contributorDeleteButton = $('<button class="danger">Remove</button>')
+      .data(user)
+      .on('click', handleContributorDelete);
+
+    $contributorListItem.append($contributorSpan, $contributorDeleteButton);
+
     return $contributorListItem;
   });
 
@@ -402,6 +411,22 @@ function openContributorModal(){
   displayMessage('');
 
   MicroModal.show('contribute');
+}
+
+function handleContributorDelete(event) {
+  let { id, email } = $(event.target).data();
+
+  $.ajax({
+    url: `/api/user_boards`,
+    method: 'DELETE',
+    data: {
+      user_id: id,
+      board_id: board.id
+    }
+  }).then(function() {
+    init();
+    displayMessage(`Successfully removed user: ${email}`, 'success');
+  });
 }
 
 
